ac604e2f5ff128e83582b80dd92ce9d8f17665c4
Žaneta Grossová <zaneta.grossova@patententer.com>
2026-01-27 12:25:32 +0100

    Přepis ceníku

--- app/kosik/page.tsx
+++ app/kosik/page.tsx
@@ -84,6 +84,7 @@ export default function KosikPage() {
 
   // init + cleanup draftů pro "na přání"
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setWishDraft((prev) => {
       const next = { ...prev };
 
