1552c8cc8fd689fce23ca54c31743532f02812cc
blejskarnastatl <blejskarnastatl@gmail.com>
2026-05-07 12:20:07 +0200

    Update page.tsx

--- app/jak_k_nam/page.tsx
+++ app/jak_k_nam/page.tsx
@@ -74,7 +74,7 @@ export default function JakKNamPage() {
                 title="Mapa Blejskárna Štatl"
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10428.069911777342!2d16.614042!3d49.200228!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47129517b5809c71%3A0xe2d9a5956b919a5!2zQmxlanNrw6FybmEgxaBUQVRMLCBydcSNbsOtIG15xI1rYSBhdXQgSUJDIEJybm8!5e0!3m2!1scs!2scz!4v1778148837753!5m2!1scs!2scz" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"/>
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10428.069911777342!2d16.614042!3d49.200228!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47129517b5809c71%3A0xe2d9a5956b919a5!2zQmxlanNrw6FybmEgxaBUQVRMLCBydcSNbsOtIG15xI1rYSBhdXQgSUJDIEJybm8!5e0!3m2!1scs!2scz!4v1778148837753!5m2!1scs!2scz"/>
             </div>
 
             {/* META */}
